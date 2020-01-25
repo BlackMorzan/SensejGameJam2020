3051907993808f26faf9945a1be079aed1dcbde7
Kperlinski <perlinski.konrad@gmail.com>
2020-01-26 00:40:14 +0100

    stage2 + hearth bar

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+
+    public int health;
+    public int numOfHearts;
+
+    public List<Image> hearts = new List<Image>();
+    public Sprite Heart64;
+    public Sprite Nheart;
+
+    void Update()
+    {
+
+        if (health > numOfHearts)
+        {
+            health = numOfHearts;
+        }
+
+        for (int i = 0; i < hearts.Count; i++)
+        {
+
+            if (i < health)
+            {
+                hearts[i].sprite = Heart64;
+            }
+            else
+            {
+                hearts[i].sprite = Nheart;
+            }
+
+            if (i < numOfHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+
+     }
+}
+
